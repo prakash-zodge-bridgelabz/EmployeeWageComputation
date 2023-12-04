575f81ee4a8fdc46be325c0b6c194d65a8daec67
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 15:02:19 +0530

    Added: UC_1_EmployeeAttendanceUC

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,7 +1,23 @@
 package com.bridgelabz;
-
+import java.util.Random;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        EmployeeWageComputation e1 = new EmployeeWageComputation();
+        e1.UC_1_EmployeeAttendanceUC();
+    }
+    public void UC_1_EmployeeAttendanceUC(){
+        Random random = new Random();
+        // Generate a random number between 0 and 1
+        int randomNumber = random.nextInt(2);
+        // Check if the random number is 0, which means the employee is present
+        if (randomNumber == 0) {
+            System.out.println("The employee is present.");
+        }
+        else {
+            // Otherwise, the employee is absent
+            System.out.println("The employee is absent.");
+
+        }
     }
 }
