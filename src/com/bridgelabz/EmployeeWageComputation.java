f9ef857ac8b010c6ae52788b6396b63c0cad1b8a
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 15:17:13 +0530

    Added: UC_4_EmployeeWageSwitchUC

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 import java.util.Random;
+import java.util.Scanner;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
@@ -7,6 +8,7 @@ public class EmployeeWageComputation {
         e1.UC_1_EmployeeAttendanceUC();
         e1.UC_2_CalculateDailyWageUC();
         e1.UC_3_CalculatePartTimeAndFullTimeWageUC();
+        e1.UC_4_EmployeeWageSwitchUC();
     }
     public void UC_1_EmployeeAttendanceUC(){
         Random random = new Random();
@@ -37,5 +39,26 @@ public class EmployeeWageComputation {
         // Calculate daily part-time employee wage
         int dailyWagePartTime = wagePerHour * partTimeHours;
         System.out.println("Daily Wage for Part-Time Employee: Rs." + dailyWagePartTime);
+    }
+    public void UC_4_EmployeeWageSwitchUC(){
+        int wagePerHour = 20;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the type of employee (1 for Full-Time, 2 for Part-Time): ");
+        int employeeType = scanner.nextInt();
+        int dailyWage;
+        switch (employeeType) {
+            case 1:
+                dailyWage = wagePerHour * 8; // Full-Time employee works 8 hours
+                System.out.println("Daily Wage for Full-Time Employee: Rs." + dailyWage);
+                break;
+
+            case 2:
+                dailyWage = wagePerHour * 4; // Part-Time employee works 4 hours
+                System.out.println("Daily Wage for Part-Time Employee: Rs." + dailyWage);
+                break;
+
+            default:
+                System.out.println("Invalid employee type entered.");
+        }
     }
 }
