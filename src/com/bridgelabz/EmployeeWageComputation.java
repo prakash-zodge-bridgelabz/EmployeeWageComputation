8a642517bba6e2f0ad7cf6050bd4f62dae5fa125
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 15:13:49 +0530

    Added: Calculate Part Time And Full Time Wage UC

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -6,6 +6,7 @@ public class EmployeeWageComputation {
         EmployeeWageComputation e1 = new EmployeeWageComputation();
         e1.UC_1_EmployeeAttendanceUC();
         e1.UC_2_CalculateDailyWageUC();
+        e1.UC_3_CalculatePartTimeAndFullTimeWageUC();
     }
     public void UC_1_EmployeeAttendanceUC(){
         Random random = new Random();
@@ -25,5 +26,16 @@ public class EmployeeWageComputation {
         int fullDayHours = 8;
         int dailyWage = wagePerHour * fullDayHours;
         System.out.println("Daily Employee Wage: Rs." + dailyWage);
+    }
+    public void UC_3_CalculatePartTimeAndFullTimeWageUC(){
+        int partTimeHours = 4;
+        int wagePerHour = 20;
+        int fullDayHours = partTimeHours*2;
+        // Calculate daily employee wage
+        int dailyWageFullTime = wagePerHour * fullDayHours;
+        System.out.println("Daily Wage for Full-Time Employee: Rs." + dailyWageFullTime);
+        // Calculate daily part-time employee wage
+        int dailyWagePartTime = wagePerHour * partTimeHours;
+        System.out.println("Daily Wage for Part-Time Employee: Rs." + dailyWagePartTime);
     }
 }
